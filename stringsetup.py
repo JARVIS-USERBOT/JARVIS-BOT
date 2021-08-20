de18ce0b3206d9b299b6c97cebf70ec8224fd87c
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-20 19:36:38 +0000

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -18,7 +18,7 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @LEGENDSupport For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @Legend_Userbot For Any Help!\n\n",
             )
 
             print(
