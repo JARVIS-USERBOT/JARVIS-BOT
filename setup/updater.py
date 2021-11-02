bfd3f3e28ab0c67dc58ab2f9a64767bd40410a69
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-02 15:35:20 +0530

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"JARVIS-USERBOT{i}")
+            print(f"JARVIS-USERBOT {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
